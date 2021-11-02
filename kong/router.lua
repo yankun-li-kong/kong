885d4795df780501150741b4a9de77b1f052fa8a
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2021-11-02 10:45:37 +0200

    perf(router) detecting the capturing groups removed (not needed)

--- kong/router.lua
+++ kong/router.lua
@@ -310,15 +310,6 @@ local function _set_ngx(mock_ngx)
 end
 
 
-local function has_capturing_groups(subj)
-  local s =      find(subj, "[^\\]%(.-[^\\]%)")
-        s = s or find(subj, "^%(.-[^\\]%)")
-        s = s or find(subj, "%(%)")
-
-  return s ~= nil
-end
-
-
 local protocol_subsystem = constants.PROTOCOLS_WITH_SUBSYSTEM
 
 
@@ -500,13 +491,11 @@ local function marshall_route(r)
 
           -- regex URI
           local strip_regex  = REGEX_PREFIX .. path .. [[(?<uri_postfix>.*)]]
-          local has_captures = has_capturing_groups(path)
 
           local uri_t    = {
             is_regex     = true,
             value        = path,
             regex        = path,
-            has_captures = has_captures,
             strip_regex  = strip_regex,
           }
 
@@ -748,7 +737,7 @@ local function sort_routes(r1, r2)
     end
   end
 
-  -- only regex path use regex_priority 
+  -- only regex path use regex_priority
   if band(r1.submatch_weight,MATCH_SUBRULES.HAS_REGEX_URI) ~= 0 then
     do
       local rp1 = r1.route.regex_priority or 0
@@ -1013,7 +1002,7 @@ do
               ctx.matches.uri = uri_t.value
               ctx.matches.uri_postfix = uri_postfix
 
-              if uri_t.has_captures then
+              if m[1] then
                 ctx.matches.uri_captures = m
               end
 
@@ -1055,7 +1044,7 @@ do
             ctx.matches.uri = uri_t.value
             ctx.matches.uri_postfix = uri_postfix
 
-            if uri_t.has_captures then
+            if m[1] then
               ctx.matches.uri_captures = m
             end
 
@@ -1279,9 +1268,6 @@ end
 
 
 local _M = {}
-
-
-_M.has_capturing_groups = has_capturing_groups
 
 
 -- for unit-testing purposes only
